d12c8b927f9760b1f2de0da5f683c3e830040cd8
honghongk <porgreany@gmail.com>
2025-09-24 21:32:24 +0900

    Fix 게시글 목록 이름 변경 index -> list

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::post('/logout', [AuthController::class, 'logout']);
 Route::middleware('auth')->group(function () {
     
     // 목록
-    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts', [PostController::class, 'list'])->name('posts.list');
 
     // 생성폼
     Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
@@ -37,7 +37,7 @@ Route::middleware('auth')->group(function () {
 
     // 수정폼
     Route::get('/posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    
+
     // 수정처리
     Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
 
